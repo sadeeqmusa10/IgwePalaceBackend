cbe020d0c0dd3ca225b3bea2b4121611325a84ea
Diannabrayn <diannabrayn@gmail.com>
2026-04-06 04:05:27 -0700

    4th commit

--- src/routes/AdminRoute.ts
+++ src/routes/AdminRoute.ts
@@ -16,7 +16,7 @@ const storage = multer.memoryStorage();
 const upload = multer({
   storage,
   limits: {
-    fileSize: 5 * 1024 * 1024, // 5MB per image
+    fileSize: 10 * 1024 * 1024, // 5MB per image
   },
 });
 
